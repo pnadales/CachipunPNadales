d0950aa9976f71ec09d51921b2b2fa7b8494ad30
Paulo Nadales <p.nadales.j@gmail.com>
2024-02-14 18:40:06 -0300

    eliminacion de ciclos e implementacion de funcion, se agregan estilos y contenido html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,16 @@
-const NumeroJugadas = Number(prompt("¿Cuantas veces deseas jugar?"));
 
-// for (let i = 0; i < NumeroJugadas; i++) {
-//     Jugada = prompt("¿Piedra, papel o tijera? Escribe tu jugada").toLowerCase();
-//     const NumeroAleatorio = Math.floor(Math.random() * 3);
-//     let JugadaOrdenador;
-//     switch (NumeroAleatorio) {
-//         case 0:
-//             JugadaOrdenador = "piedra";
-//             break;
-//         case 1:
-//             JugadaOrdenador = "papel";
-//             break;
-//         case 2:
-//             JugadaOrdenador = "tijera";
-//             break;
+$(".juego").hide();
+let NumeroJugadas
+$("#comenzar").click(NumeroJugadas = function () {
+    $(".comienzos").hide();
+    NumeroJugadas = Number(prompt("¿Cuantas veces deseas jugar?"));
+    $(".juego").show();
 
-//     }
-
-//     let Resultado;
-//     if (JugadaOrdenador == Jugada) {
-//         Resultado = "Empataron";
-//     } else if (JugadaOrdenador == "tijera") {
-//         if (Jugada == "papel") {
-//             Resultado = "Ordenador gana";
-//         } else {
-//             Resultado = "Ganaste";
-//         }
-//     } else if (JugadaOrdenador == "piedra") {
-//         if (Jugada == "tijera") {
-//             Resultado = "Ordenador gana";
-
-//         } else {
-//             Resultado = "Ganaste";
-//         }
-//     } else if (JugadaOrdenador == "papel") {
-//         if (Jugada == "piedra") {
-//             Resultado = "Ordenador gana";
-//         } else {
-//             Resultado = "Ganaste";
-//         }
-
-//     }
-
-//     document.write("Tu jugada fue " + Jugada + " la jugada del ordenador fue " + JugadaOrdenador + ", entonces " + Resultado + "<br>");
-// }
-
-
-// let i = 0;
-
-// while (i < NumeroJugadas) {
-//     Jugada = prompt("¿Piedra, papel o tijera? Escribe tu jugada").toLowerCase();
-//     const NumeroAleatorio = Math.floor(Math.random() * 3);
-//     let JugadaOrdenador;
-//     switch (NumeroAleatorio) {
-//         case 0:
-//             JugadaOrdenador = "piedra";
-//             break;
-//         case 1:
-//             JugadaOrdenador = "papel";
-//             break;
-//         case 2:
-//             JugadaOrdenador = "tijera";
-//             break;
-
-//     }
-
-//     let Resultado;
-//     if (JugadaOrdenador == Jugada) {
-//         Resultado = "Empataron";
-//     } else if (JugadaOrdenador == "tijera") {
-//         if (Jugada == "papel") {
-//             Resultado = "Ordenador gana";
-//         } else {
-//             Resultado = "Ganaste";
-//         }
-//     } else if (JugadaOrdenador == "piedra") {
-//         if (Jugada == "tijera") {
-//             Resultado = "Ordenador gana";
-
-//         } else {
-//             Resultado = "Ganaste";
-//         }
-//     } else if (JugadaOrdenador == "papel") {
-//         if (Jugada == "piedra") {
-//             Resultado = "Ordenador gana";
-//         } else {
-//             Resultado = "Ganaste";
-//         }
-
-//     }
-
-//     document.write("Tu jugada fue " + Jugada + " la jugada del ordenador fue " + JugadaOrdenador + ", entonces " + Resultado + "<br>");
-//     i++;
-// }
+});
 
 let contador = 0;
+let Jugador = 0;
+let Maquina = 0;
 
 function cachipun(Jugada, N, cont) {
 
@@ -137,13 +53,25 @@ function cachipun(Jugada, N, cont) {
         }
 
     }
+    if (Resultado == "Ganaste") {
+        Jugador++;
+    } else if (Resultado == "Ordenador gana") {
+        Maquina++;
+    }
 
-    alert("Tu jugada fue " + Jugada + " la jugada del ordenador fue " + JugadaOrdenador + ", entonces " + Resultado);
+    alert("La máquina jugó" + JugadaOrdenador + ", entonces " + Resultado);
 
     cont++;
     if (cont == N) {
-
+        if (Maquina < Jugador) {
+            alert("Has ganado la partida!!!")
+        } else if (Maquina > Jugador) {
+            alert("Has perdido la partida :c")
+        } else {
+            alert("Empate ._.")
+        }
         $(".juego").hide();
     }
     return cont;
 }
+
